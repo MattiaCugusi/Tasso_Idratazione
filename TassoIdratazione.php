51258405596068d6ed97105bb653e94b59de1234
Mattia Cugusi <m.cugusi287gmail.com>
2024-09-25 18:25:08 +0200

    aggiunta script php

--- TassoIdratazione.php
+++ TassoIdratazione.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tasso d'idratazione</title>
 </head>
 <body>
+    <?php
+      $tasso;
+      $peso = 60;
+      $millilitri = 30;
+
+      echo "<h1>Mattia Cugusi</h1>";
+
+      echo "<p>Il tasso di idratazione di una persona che pesa " . $peso . "kg e' di " . $tasso;
+
+    ?>
     
 </body>
 </html>
